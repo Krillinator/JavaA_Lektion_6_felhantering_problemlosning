a1905ab39f68371b68e4bc75bd8649c8ab7540d4
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-09-28 10:40:04 +0200

    Clean slate

--- src/com/kristoffer/felprob/Main.java
+++ src/com/kristoffer/felprob/Main.java
@@ -5,42 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int[] numbers = {0, 100, 1000, 2000};
-
-
-
-        System.out.println("Before crash");
-
-        try {
-            // Try to execute code
-            System.out.println("Inside - TRY ");
-            System.out.println(2/0);
-
-        } catch (Exception error) {
-            // Catch error if it FAILS
-            System.out.println("Inside - CATCH ");
-            error.printStackTrace();
-        }
-
-        System.out.println("After crash");
-
-
-
-
-
-
-
-        /*
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Type something to crash the program");
-            scanner.nextLine();
-        */
-
-        /*
-            for (int i = 0; i < numbers.length + 1; i++) {
-                System.out.println(numbers[i]);
-            }
-        */
 
     }
 }
